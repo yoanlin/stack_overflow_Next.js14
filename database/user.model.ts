ba9f3de54485550b7b5ba7e9d90aa80a1a78c460
Yoan Lin <yoanlin068@gmail.com>
2024-08-22 23:16:21 +0800

    fix

--- database/user.model.ts
+++ database/user.model.ts
@@ -27,7 +27,7 @@ const UserSchema = new Schema({
   protfolioWebsite: { type: String },
   reputation: { type: Number, default: 0 },
   saved: [{ type: Schema.Types.ObjectId, ref: "Question" }],
-  joinedAt: { type: Date },
+  joinedAt: { type: Date, default: Date.now },
 });
 
 const User = models.User || model("User", UserSchema);
