b8e0a89514b156cefff579382d767fd3c072037c
Yoan Lin <yoanlin068@gmail.com>
2024-08-23 01:12:38 +0800

    complete webhook

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -5,14 +5,12 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
-  const userId = "clerk12345";
+  const { userId } = auth();
 
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
 
-  console.log(mongoUser);
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
